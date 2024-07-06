ef74bf2a86a2105f365ae38ade0bc1d427063898
Kapil1721 <107835784+Kapil1721@users.noreply.github.com>
2024-07-06 17:40:25 +0530

    Update app.js
    undo

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.get("/api/v1", (req, res) => {
   res.status(200).json({ message: "I'am fine" });
 });
 
-app.use("", userRoutes);
+app.use("/api/v1", userRoutes);
 
 // Handle undefined routes
 app.all("*", (req, res, next) => {
